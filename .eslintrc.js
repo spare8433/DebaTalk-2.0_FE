c8b6866c78d9c6704bb43d6b00c59a4a22c07363
ByeongChan <thdqudcks2@gmail.com>
2023-06-16 20:18:55 +0900

    update eslint rule 'jsx-a11y'

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,12 @@ module.exports = {
     'react/jsx-props-no-spreading': 'warn', // props를 펼침연산자(spread operator)를 쓰는 것 제한
     'react/require-default-props': 'off', //  default props 설정해줘야 하는 것 제거
     'react/no-array-index-key': 'warn', // 배열안에서 index 값을 keys 로 쓰는 것을 제한
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor'],
+      },
+    ],
     '@typescript-eslint/semi': 'off',
     '@typescript-eslint/ban-types': 'warn', // 직접적으로 type 을 {} 로 주는 것을 제한
     '@typescript-eslint/indent': 'off',
