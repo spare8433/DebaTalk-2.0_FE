743dc0fd8d491d1cd4c83515070a2102e27891bb
ByeongChan <thdqudcks2@gmail.com>
2023-06-20 18:46:29 +0900

    update easy to improt customeCssType

---
+++ types/customCssType/index.ts
@@ -0,0 +1,11 @@
+import CssValue from './cssValue'
+import CssRem from './cssRem'
+import CssEm from './cssEm'
+import CssNone from './cssNone'
+import CssString from './cssString'
+import CssPixel from './cssPixel'
+import CssPercent from './cssPercent'
+
+export { CssRem, CssEm, CssNone, CssPercent, CssPixel, CssString }
+
+export type { CssValue }
