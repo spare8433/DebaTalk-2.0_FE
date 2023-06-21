e39cdb122c82665dde07fcef721b0aee8dfc9132
ByeongChan <thdqudcks2@gmail.com>
2023-06-21 18:57:34 +0900

    update simple things of common/mainSearch

--- components/common/mainSearch/index.tsx
+++ components/common/mainSearch/index.tsx
@@ -1,15 +1,17 @@
 import useInput from '@hooks/useInput'
 import { getIntegrateDebatePosts } from '@store/slices/debatePosts'
 import { useAppDispatch } from '@store/store'
-import { ImgBox } from '@styles/commonStyle'
 import React, { useCallback } from 'react'
+import { NextImageBox } from '@styles/commonStyle/imgBox'
+import { CssRem } from 'types/customCssType'
 import { MainSearchBox } from './style'
+import FitNextImage from '../fitNextImage'
 
 type WrapperProps = {
-	searchText: string;
+  searchText: string
 }
 
-const MainSearch = ({searchText}:WrapperProps) => {
+const MainSearch = ({ searchText }: WrapperProps) => {
   const dispatch = useAppDispatch()
   const [text, changeText] = useInput(searchText)
 
@@ -17,25 +19,24 @@ const MainSearch = ({searchText}:WrapperProps) => {
     try {
       dispatch(getIntegrateDebatePosts()).unwrap()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  }, [text])
+  }, [dispatch])
 
   return (
-    <>
-      <MainSearchBox>
-        <ImgBox><img alt='돋보기' src='./img/search.png'></img></ImgBox>
-        <form onSubmit={intergrateSearch}>
-          <input 
-            onChange={changeText} 
-            placeholder='관심있는 내용을 검색해보세요'
-          >
-            {text}
-          </input>
-        </form>
-         
-      </MainSearchBox>
-    </>
+    <MainSearchBox>
+      <NextImageBox styleOption={{ width: new CssRem(1.9), height: new CssRem(1.9) }}>
+        <FitNextImage alt="돋보기" src="/img/search.png" />
+      </NextImageBox>
+      <form onSubmit={intergrateSearch}>
+        <input
+          type="text"
+          onChange={changeText}
+          placeholder="관심있는 내용을 검색해보세요"
+          value={text}
+        />
+      </form>
+    </MainSearchBox>
   )
 }
 
--- components/common/mainSearch/style.ts
+++ components/common/mainSearch/style.ts
@@ -1,28 +1,25 @@
-import { ImgBox } from "@styles/commonStyle";
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const MainSearchBox = styled.div`
   width: 100%;
   background-color: white;
   box-sizing: border-box;
-  border: #A38AF8 4px solid;
-  border-radius:25px;
+  border: ${({ theme }) => theme.colors.main} 0.4rem solid;
+  border-radius: 2.5rem;
   display: flex;
   align-items: center;
-  height: 45px;
-  ${ImgBox}{
-    /* margin: 0 8px; */
-    margin-left: 12px;
-  }
-  input{
+  height: 4.5rem;
+  padding: 0 1.2rem;
+
+  input {
     width: 100%;
     border: 0;
     padding: 0;
-    margin: 0 12px;
+    margin: 0 1.2rem;
     outline: none;
     height: 100%;
     background: none;
-    font-size: 16px;
+    font-size: 1.6rem;
     font-weight: 400;
   }
 `
