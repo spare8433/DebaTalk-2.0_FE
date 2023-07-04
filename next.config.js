a7df32e67ce52d96427de0d9d1c2f55b2f2d74b4
ByeongChan <thdqudcks2@gmail.com>
2023-07-04 20:54:23 +0900

    swc test update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName: true,
+      // Enabled by default.
+      ssr: true,
+      // Not supported yet.
+      pure: true,
+    },
+  },
+}
