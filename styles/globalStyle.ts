f11c07af4e75766f1f2ce62ff91623f4f2c2ee33
ByeongChan <thdqudcks2@gmail.com>
2023-07-04 21:01:43 +0900

    update globalStyle

--- styles/globalStyle.ts
+++ styles/globalStyle.ts
@@ -1,9 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
+// import { Noto_Sans_KR } from 'next/font/google'
+
+// const NotoSansKR = Noto_Sans_KR({
+//   weight: ['100', '300', '400', '500', '700', '900'],
+//   style: 'normal',
+//   subsets: ['latin'],
+//   preload: true,
+//   fallback: ['Arial'],
+// })
 
 const GlobalStyles = createGlobalStyle`
     ${normalize}
     * {
+      font-family: 'Noto Sans KR', sans-serif;
+      font-weight: 400;
       box-sizing: border-box;
       color: #3b3b3b;
     }
@@ -14,15 +25,20 @@ const GlobalStyles = createGlobalStyle`
     html {font-size: 10px;}
     body {
       background-color: #F5F5F5;
-      font-weight: 400;
       box-sizing: border-box;
-      font-family: 'Noto Sans KR', sans-serif;   
     }
     h1,h2,h3,h4,h5,h6,p {
       margin: 0;
+      font-weight: 500;
     }
-    h1 {font-size: 2.4rem}
-    h2 {font-size: 2.2rem}
+    h1 {
+      font-size: 2.4rem;
+      font-weight: 700;
+    }
+    h2 {
+      font-size: 2.2rem;
+      font-weight: 700;
+    }
     h3 {font-size: 2.0rem}
     h4 {font-size: 1.8rem}
     h5 {font-size: 1.6rem}
@@ -43,9 +59,6 @@ const GlobalStyles = createGlobalStyle`
     ul {
       padding: 0;
       margin: 0;
-    }
-    select,input,textarea{
-      font-family: 'Noto Sans KR', sans-serif;   
     }
 `
 
