68888ed796a55a760e097aa076b37d4e35de9174
ByeongChan <thdqudcks2@gmail.com>
2023-08-01 20:39:08 +0900

    API URL test

--- api/index.ts
+++ api/index.ts
@@ -9,6 +9,8 @@ axios.defaults.withCredentials = true
 const { publicRuntimeConfig } = getConfig()
 const APISeverUrl = publicRuntimeConfig.API_SERVER_URL
 
+console.log(APISeverUrl)
+
 export const axiosInstance = (url: string) => axios.create({ baseURL: `${APISeverUrl}${url}` })
 
 // export const axiosInstance = (url: string) => axios.create({ baseURL: `${1}${url}` })
